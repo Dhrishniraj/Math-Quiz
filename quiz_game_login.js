3bc6cb74a6cfa644c521bd1725feef26b31dbedf
Dhrishniraj <83451121+Dhrishniraj@users.noreply.github.com>
2021-11-10 18:15:23 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,30 @@
+function local() {
+    document.getElementById("player1_name_input").value=localStorage.getItem("player1_name1");
+    document.getElementById("player2_name_input").value=localStorage.getItem("player2_name1");
+    console.log("Player 1 name = " + localStorage.getItem("player1_name1"));
+    console.log("Player 2 name = " + localStorage.getItem("player2_name1"));
+}
+
+function adduser() {
+    var p1 = document.getElementById("player1_name_input").value;
+    var p2 = document.getElementById("player2_name_input").value;
+
+    if((p1.length >= 1) && (p2.length >= 1)) {
+        var player1_name = document.getElementById("player1_name_input").value;
+        var player2_name = document.getElementById("player2_name_input").value;
+        var player1 = player1_name;
+        var player2 = player2_name;
+        localStorage.setItem("player1_name1", player1);
+        localStorage.setItem("player2_name1", player2);
+        window.location = "quiz_game.html";
+    }
+    else if((p1.length < 1) && (p2.length >= 1)){
+        window.alert("Player 1 name input box is empty. You are requested to fill it.");
+    }
+    else if((p2.length < 1) && (p1.length >= 1)){
+        window.alert("Player 2 name input box is empty. You are requested to fill it.");
+    }
+    else if((p1.length < 1) && (p2.length < 1)){
+        window.alert("Player name inputs are empty. You are requested to fill them.");
+    }
+}
